a88bb759b25dc2cbe1c66bab705e532f53dac9b9
NeiJunior <Nei Junior@DESKTOP-SS8QBJE>
2025-11-14 17:44:09 -0300

    primeiro

---
+++ src/backend/src/PeladaPatronato.Domain/Entidades/EventoPartida.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeladaPatronato.Domain.Entidades
+{
+  public class EventoPartida
+  {
+    public Guid Id { get; set; }
+    public Guid PartidaId { get; set; }
+    public Guid ParticipanteId { get; set; }
+    public eTipoEvento TipoEvento { get; set; }
+
+    public enum eTipoEvento
+    {
+      Assistencia = 1,
+      Gol = 2
+    }
+  }
+}
---
+++ src/backend/src/PeladaPatronato.Domain/Entidades/Participante.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeladaPatronato.Domain.Entidades
+{
+  public class Participante
+  {
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string Apelido { get; set; }
+    public ePosicao PosicaoPreferida { get; set; }
+    public bool Ativo { get; set; }
+
+    public Participante()
+    {
+      
+    }
+
+    public enum ePosicao
+    {
+      Goleiro = 1,
+      Fixo = 2,
+      Ala = 3,
+      Pivo = 4
+    }
+  }
+}
